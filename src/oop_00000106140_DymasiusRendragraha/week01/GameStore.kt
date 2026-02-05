c7f14a41454bc3158bc58748ed0e94f09708397e
Miaki <goldenation139@gmail.com>
2026-02-05 14:47:53 +0700

    First commit

---
+++ src/oop_00000106140_DymasiusRendragraha/week01/GameStore.kt
@@ -0,0 +1,9 @@
+package oop_00000106140_DymasiusRendragraha.week01
+
+fun main(){
+    var name : String = "John Thor"
+    var score: Int = 80
+
+    //Concatenation gaya lama
+    println("Nama: " + name + " Score: " + score)
+}
---
+++ src/oop_00000106140_DymasiusRendragraha/week01/GradingSystem.kt
@@ -0,0 +1,2 @@
+package oop_00000106140_DymasiusRendragraha.week01
+
