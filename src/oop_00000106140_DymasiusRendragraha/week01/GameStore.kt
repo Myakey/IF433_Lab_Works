2b300d347f1168abbc76ccdf4de17a5ee9f422cf
Miaki <goldenation139@gmail.com>
2026-02-07 21:19:26 +0700

    week01: (tugas 2) implement null safety for user note

--- src/oop_00000106140_DymasiusRendragraha/week01/GameStore.kt
+++ src/oop_00000106140_DymasiusRendragraha/week01/GameStore.kt
@@ -5,15 +5,16 @@ import kotlin.math.roundToInt
 fun main(){
     val gameTitle: String = "Persona 5 Reload"
     val price: Int = 300000
+    val userNote: String? = null
 
     val finalPrice: Int = price - calculateDiscount(price).roundToInt()
 
-    //print recepit
-    printReceipt(gameTitle, finalPrice)
+    printReceipt(gameTitle, finalPrice, userNote)
 }
 
 fun calculateDiscount(price: Int) = if(price > 500000) price * 0.2 else price * 0.1
 
-fun printReceipt(title: String, finalPrice: Int) {
-    println("Title: $title, final Price: $finalPrice")
+fun printReceipt(title: String, finalPrice: Int, userNote: String?) {
+    val notes = userNote ?: "Tidak ada catatan"
+    println("Title: $title, final Price: $finalPrice, user note: $notes")
 }
