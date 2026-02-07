e07e3be75b66be25941c4af60a68d764145c956b
Miaki <goldenation139@gmail.com>
2026-02-07 21:13:38 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000106140_DymasiusRendragraha/week01/GameStore.kt
+++ src/oop_00000106140_DymasiusRendragraha/week01/GameStore.kt
@@ -1,8 +1,18 @@
 package oop_00000106140_DymasiusRendragraha.week01
 
+import kotlin.math.roundToInt
+
 fun main(){
     val gameTitle: String = "Persona 5 Reload"
     val price: Int = 300000
+
+    val finalPrice: Int = price - calculateDiscount(price).roundToInt()
+
+    printReceipt(gameTitle, finalPrice)
 }
 
-fun calculateDiscount(price: Int) = if(price > 500000) price * 0.2 else price * price * 0.1
+fun calculateDiscount(price: Int) = if(price > 500000) price * 0.2 else price * 0.1
+
+fun printReceipt(title: String, finalPrice: Int) {
+    println("Title: $title, final Price: $finalPrice")
+}
