7d2420bb2d65b744215aaf03c9e55cb5b9a1cf94
Miaki <goldenation139@gmail.com>
2026-02-07 21:05:53 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000106140_DymasiusRendragraha/week01/GameStore.kt
+++ src/oop_00000106140_DymasiusRendragraha/week01/GameStore.kt
@@ -4,3 +4,5 @@ fun main(){
     val gameTitle: String = "Persona 5 Reload"
     val price: Int = 300000
 }
+
+fun calculateDiscount(price: Int) = if(price > 500000) price * 0.2 else price * price * 0.1
