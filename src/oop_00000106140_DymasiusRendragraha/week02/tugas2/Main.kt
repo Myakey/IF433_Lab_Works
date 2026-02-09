d73897962e210bb501773df7c66ff0c9622c56ef
Miaki <goldenation139@gmail.com>
2026-02-09 16:11:06 +0700

    week02: (task 2) implement the game

--- src/oop_00000106140_DymasiusRendragraha/week02/tugas2/Main.kt
+++ src/oop_00000106140_DymasiusRendragraha/week02/tugas2/Main.kt
@@ -7,14 +7,48 @@ fun main(){
 
     println("=== GAME RPG SEDERHANA ===");
 
-    print("Masukkan Judul: ");
-    val bookTitle = scanner.nextLine();
+    print("Masukkan Nama Hero: ");
+    val heroName = scanner.nextLine();
 
-    print("Masukkan Peminjam: ");
-    val borrower = scanner.nextLine();
-
-    print("Masukkan Lama Pinjam: ");
-    var loanDuration = scanner.nextInt();
+    print("Masukkan Stat Damage: ");
+    val statDamage = scanner.nextInt();
 
     scanner.nextLine();
+
+    val Hero = Hero(heroName, statDamage);
+
+    var enemyHP = 100;
+
+    while(Hero.isAlive() && enemyHP > 0){
+        print("Musuh terlihat!:\n1. Serang\n2. Kabur\n");
+        print("Pilihan: ");
+        val pilihan = scanner.nextInt();
+        scanner.nextLine();
+
+        when (pilihan) {
+            1 -> {
+                Hero.attack("Musuh");
+                enemyHP -= Hero.baseDamage;
+                println("Sisa HP Musuh: $enemyHP");
+
+                if (enemyHP > 0) {
+                    Hero.takeDamage((10..20).random());
+                    println("Sisa HP Hero: ${Hero.hp}");
+                } else {
+                    break;
+                }
+            }
+            2 -> {
+                println("Kabur!");
+                break;
+            }
+            else -> print("Salah opsi!")
+        }
+    }
+
+    if (enemyHP > 0) {
+        println("Musuh menang!");
+    } else {
+        println("Kamu menang!");
+    }
 }
