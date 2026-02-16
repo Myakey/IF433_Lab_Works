76d102bcd46be7a0a538ee5695e6dd599f6a8f39
Miaki <goldenation139@gmail.com>
2026-02-16 21:59:45 +0700

    week03: cleanup code

--- src/oop_00000106140_DymasiusRendragraha/week03/Main.kt
+++ src/oop_00000106140_DymasiusRendragraha/week03/Main.kt
@@ -3,7 +3,6 @@ package oop_00000106140_DymasiusRendragraha.week03
 fun main(){
     val e = Employee("Budi")
 
-    e.salary = -1000 //test negatif
     e.salary = 50000000
     println("Gaji: ${e.salary}")
 
