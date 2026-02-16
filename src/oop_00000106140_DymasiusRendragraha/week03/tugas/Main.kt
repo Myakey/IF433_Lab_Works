515fe2580ccfe3d9824e81b4ef11a9bf28886c2a
Miaki <goldenation139@gmail.com>
2026-02-16 22:25:12 +0700

    week03: (task 1) create Weapon class with validation

---
+++ src/oop_00000106140_DymasiusRendragraha/week03/tugas/Main.kt
@@ -0,0 +1,5 @@
+package oop_00000106140_DymasiusRendragraha.week03.tugas
+
+fun main() {
+    val weapon = Weapon("Pedang");
+}
--- src/oop_00000106140_DymasiusRendragraha/week03/tugas/Weapon.kt
+++ src/oop_00000106140_DymasiusRendragraha/week03/tugas/Weapon.kt
@@ -2,8 +2,17 @@ package oop_00000106140_DymasiusRendragraha.week03.tugas
 
 class Weapon (
     val name: String,
-    var damage: Int
 ){
+    var damage: Int = 0
+        set(value){
+            if (value > 0) {
+                println("Weapon $name set damage jadi $value")
+                field = value
+            } else {
+                println("Damage tidak boleh negatif!")
+                field = 0
+            }
+        }
     val tier: String
         get() = if (damage > 800) "Legendary" else if (damage > 500) "Epic" else "Common"
 }
