8357c8d086a968de82b1fb26f8cc8ae3c549625d
Miaki <goldenation139@gmail.com>
2026-02-16 21:54:34 +0700

    week03: add private property

--- src/oop_00000106140_DymasiusRendragraha/week03/Employee.kt
+++ src/oop_00000106140_DymasiusRendragraha/week03/Employee.kt
@@ -1,7 +1,7 @@
 package oop_00000106140_DymasiusRendragraha.week03
 
 class Employee (val name: String) {
- var salary: Int = 0
+    var salary: Int = 0
      set(value) {
          println("Mecoba set gaji ke : $value")
          //Kode di bawah ini sangat berbahaya
@@ -14,4 +14,18 @@ class Employee (val name: String) {
              field = value // Field untuk assign nilai asli
          }
      }
+    private var performanceRating: Int = 3
+
+    fun increasePerformance() {
+        performanceRating++
+        println("Kinerja $name meningkat! Rating: $performanceRating")
+    }
+
+    //Tidak membuat getter untuk performance rating, jadi data ini benar benar rahasia
+    //Kecuali buat function khusus untuk print
+
+    fun printStatus() {
+        println("Karyawan: $name, Rating: $performanceRating")
+    }
+
 }
