f4df3d112a7e54395696eb5ac460d94fadf84fd1
Miaki <goldenation139@gmail.com>
2026-02-16 21:48:19 +0700

    week03: simulate stack overflow error in setter

--- src/oop_00000106140_DymasiusRendragraha/week03/Employee.kt
+++ src/oop_00000106140_DymasiusRendragraha/week03/Employee.kt
@@ -2,4 +2,10 @@ package oop_00000106140_DymasiusRendragraha.week03
 
 class Employee (val name: String) {
  var salary: Int = 0
+     set(value) {
+         println("Mecoba set gaji ke : $value")
+         //Kode di bawah ini sangat berbahaya
+         //Memanggil setter di dalam setter
+         this.salary = value;
+     }
 }
