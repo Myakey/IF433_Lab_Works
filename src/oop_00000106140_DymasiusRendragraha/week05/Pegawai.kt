461286f4ffdef2dbc1041c0f4bcbb64ad86438fa
Miaki <goldenation139@gmail.com>
2026-03-02 15:19:25 +0700

    week05: create abstract class Pegawai

---
+++ src/oop_00000106140_DymasiusRendragraha/week05/Pegawai.kt
@@ -0,0 +1,6 @@
+package oop_00000106140_DymasiusRendragraha.week05
+
+abstract class Pegawai(val nama: String) {
+    //Abstract function tidak memiliki body (Tanpa kurung kurawal)
+    abstract fun bekerja()
+}
