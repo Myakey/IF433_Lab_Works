0ec97fe50707f549409d2ab24bf3dd858548a948
Miaki <goldenation139@gmail.com>
2026-03-02 16:10:56 +0700

    week05: (task 2) create abstract PaymentMethod

---
+++ src/oop_00000106140_DymasiusRendragraha/week05/EWallet.kt
@@ -0,0 +1,8 @@
+package oop_00000106140_DymasiusRendragraha.week05
+
+class EWallet() : PaymentMethod {
+    var Balance: Double = 0.0
+    override fun processPayment(amount: Double) {
+
+    }
+}
--- src/oop_00000106140_DymasiusRendragraha/week05/PaymentMethod.kt
+++ src/oop_00000106140_DymasiusRendragraha/week05/PaymentMethod.kt
@@ -1,6 +1,5 @@
 package oop_00000106140_DymasiusRendragraha.week05
 
-abstract class PaymentMethod {
-    abstract val accountName: String
+abstract class PaymentMethod (val accountName: String) {
     abstract fun processPayment(amount: Double)
 }
