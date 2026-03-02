516805ad8ae807cb1db7dc436f7727ab5ca9df6a
Miaki <goldenation139@gmail.com>
2026-03-02 16:20:45 +0700

    week05: (task 2) create CreditCard class

--- src/oop_00000106140_DymasiusRendragraha/week05/CreditCard.kt
+++ src/oop_00000106140_DymasiusRendragraha/week05/CreditCard.kt
@@ -1,7 +1,13 @@
 package oop_00000106140_DymasiusRendragraha.week05
 
 class CreditCard(accountName: String, val limit: Double) : PaymentMethod(accountName) {
+    var usedAmount: Double = 0.0
     override fun processPayment(amount: Double) {
-
+        if (usedAmount + amount <= limit) {
+            usedAmount += amount
+            println("Transaksi berhasil!")
+        } else {
+            println("Transaksi ditolak!")
+        }
     }
 }
