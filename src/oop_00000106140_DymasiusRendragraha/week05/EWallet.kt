1f547fa7446bb123dc38876850ec5ad08bf73ea9
Miaki <goldenation139@gmail.com>
2026-03-02 16:14:40 +0700

    week05: (task 2) create EWallet class

--- src/oop_00000106140_DymasiusRendragraha/week05/EWallet.kt
+++ src/oop_00000106140_DymasiusRendragraha/week05/EWallet.kt
@@ -1,8 +1,17 @@
 package oop_00000106140_DymasiusRendragraha.week05
 
-class EWallet() : PaymentMethod {
-    var Balance: Double = 0.0
+class EWallet(accountName: String) : PaymentMethod(accountName) {
+    var balance: Double = 0.0
     override fun processPayment(amount: Double) {
+        if (balance >= amount) {
+            balance -= amount
+           println("Cukup dan sukses!")
+        } else {
+            println("Tidak cukup!")
+        }
+    }
 
+    fun topUp(amount: Double) {
+        balance += amount
     }
 }
