024da62193e4e249ac1e202124cfcbf470722a54
Miaki <goldenation139@gmail.com>
2026-03-09 15:04:11 +0700

    week06: create compound interfaces

---
+++ src/oop_00000106140_DymasiusRendragraha/week06/Capabilities.kt
@@ -0,0 +1,10 @@
+package oop_00000106140_DymasiusRendragraha.week06
+
+//interface = can-do (Behavior)
+interface BluetoothConnectable {
+    fun connectToBluetooth()
+}
+
+interface Rechargeable {
+    fun chargeBattery()
+}
