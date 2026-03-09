79cb5ca1f0d7ff91a609feecd8fac22c4be32b3c
Miaki <goldenation139@gmail.com>
2026-03-09 15:55:33 +0700

    week06: (task) implement advanced smart casting in Hub

--- src/oop_00000106140_DymasiusRendragraha/week06/SmartHomeHub.kt
+++ src/oop_00000106140_DymasiusRendragraha/week06/SmartHomeHub.kt
@@ -12,4 +12,16 @@ class SmartHomeHub(val devices: MutableList<SmartDevice> = mutableListOf()) {
             }
         }
     }
+
+    fun activateSecurityMode() {
+        for(device in devices) {
+            if (device is Recordable) {
+                device.startRecord()
+            }
+
+            if (device is SmartSpeaker) {
+                device.playMusic("Sirine Peringatan")
+            }
+        }
+    }
 }
