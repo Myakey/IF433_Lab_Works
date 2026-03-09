86912df169d4ebac106e727fcb48652011e826c1
Miaki <goldenation139@gmail.com>
2026-03-09 15:34:09 +0700

    week06: (task) create smart home interfaces

---
+++ src/oop_00000106140_DymasiusRendragraha/week06/SmartInterfaces.kt
@@ -0,0 +1,16 @@
+package oop_00000106140_DymasiusRendragraha.week06
+
+interface SmartDevice {
+    val id: String
+    val name: String
+}
+
+interface Switchable {
+    fun turnOn()
+    fun turnOff()
+}
+
+interface Recordable {
+    fun startRecord()
+    fun stopRecord()
+}
