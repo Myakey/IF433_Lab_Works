19198bf9a8697b40d949fcbd5fd55dabd18d903c
Myakey <goldenation139@gmail.com>
2026-04-13 15:38:43 +0700

    week08: create LegacyJavaAPI class in Java

---
+++ src/oop_00000106140_DymasiusRendragraha/week08/LegacyJavaAPI.java
@@ -0,0 +1,8 @@
+package oop_00000106140_DymasiusRendragraha.week08;
+
+public class LegacyJavaAPI {
+    //Fungsi ini ditulis dalam java. Kotlin tidak tahu apakah kembaliannya nullable.
+    public static String fetchServerStatus() {
+        return "SERVER_ONLINE_200_OK";
+    }
+}
