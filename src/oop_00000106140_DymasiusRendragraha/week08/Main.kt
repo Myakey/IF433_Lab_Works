913a103bcd8ac5afe97b2aa94020ab3194b0dcd9
Myakey <goldenation139@gmail.com>
2026-04-13 15:13:37 +0700

    week08: implement chained safe calls and elvis

--- src/oop_00000106140_DymasiusRendragraha/week08/Main.kt
+++ src/oop_00000106140_DymasiusRendragraha/week08/Main.kt
@@ -1,5 +1,10 @@
 package oop_00000106140_DymasiusRendragraha.week08 
 
 fun main() {
-    println("Test succeed!")
+    println("=== TEST SAFE CALLS & ELVIS ===")
+    val emptyOrder = Order(null, null)
+
+    // Rantai safe calls yang elegan
+    val destination = emptyOrder.deliveryDetails?.address?.city?.name ?: "Kota Tidak Diketahui"
+    println("Tujuan pengiriman: $destination")
 }
