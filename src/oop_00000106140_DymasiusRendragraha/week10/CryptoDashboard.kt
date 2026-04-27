67df9c82a52ea097ba5e4862eda6a4190f461c72
Miaki <goldenation139@gmail.com>
2026-04-27 13:33:02 +0700

    week10: (task) populate coin repository

--- src/oop_00000106140_DymasiusRendragraha/week10/CryptoDashboard.kt
+++ src/oop_00000106140_DymasiusRendragraha/week10/CryptoDashboard.kt
@@ -2,4 +2,10 @@ package oop_00000106140_DymasiusRendragraha.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("BTC", 2.5453))
+    coinRepo.add(Coin("ETH", 1.25))
+    coinRepo.add(Coin("USDT", 500.0))
+
+
 }
