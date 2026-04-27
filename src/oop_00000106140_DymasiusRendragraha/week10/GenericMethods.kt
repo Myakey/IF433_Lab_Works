b5a3aea5a869e9ba7a95af7548cfc3b5ebc82a77
Miaki <goldenation139@gmail.com>
2026-04-27 13:11:31 +0700

    week10: implement basic generic function printData

--- src/oop_00000106140_DymasiusRendragraha/week10/GenericMethods.kt
+++ src/oop_00000106140_DymasiusRendragraha/week10/GenericMethods.kt
@@ -1,5 +1,9 @@
 package oop_00000106140_DymasiusRendragraha.week10
 
-fun <T> printDat(data: T) {
+fun <T> printData(data: T) {
     println("Data yang diterima: $data")
+}
+
+fun <T> processData(input: T) : T {
+    return input
 }
